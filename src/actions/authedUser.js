2395f4baa3c0211a5183658c5bd735827fbea000
Michel Emad Wakim <micho.courses@gmail.com>
2021-01-05 19:35:58 +0200

    adding the receiving qustions actions

---
+++ src/actions/authedUser.js
@@ -0,0 +1,10 @@
+//creatina a function to set the user
+
+export const SET_AUTHED_USER = 'SET_AUTHED_USER'
+
+export function setAuthedUser (id) {
+  return {
+    type: SET_AUTHED_USER,
+    id,
+  }
+}
